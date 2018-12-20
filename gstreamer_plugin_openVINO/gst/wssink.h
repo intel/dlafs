02cbed059592c618c7d9d9733763ab86ecc1625c
lijunjie <river.li@intel.com>
2018-12-20 17:54:06 +0800

    (gst_plugin) add license for gst plugin and hal
    HDDL-S pipeline: MIT license
    HDDL-S GStreamer plugins: LGPL v2.1 license
    
    1. Add license header to all cpp/h files
    2. Add COPYING into each folder
    
    Change-Id: If857dbc3f1f816ea5f1daf7bdf144779997c18c7
    Signed-off-by: Li, River <river.li@intel.com>

--- gstreamer_plugin_openVINO/gst/wssink.h
+++ gstreamer_plugin_openVINO/gst/wssink.h
@@ -1,24 +1,25 @@
-/* GStreamer
- * Copyright (C) 1999,2000 Erik Walthinsen <omega@cse.ogi.edu>
- *                    2000 Wim Taymans <wtay@chello.be>
+/*
+ *Copyright (C) 2018 Intel Corporation
  *
- * gstwssink.h:
+ *SPDX-License-Identifier: LGPL-2.1-only
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Library General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
+ *This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation;
+ * version 2.1.
  *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Library General Public License for more details.
+ *This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Library General Public
- * License along with this library; if not, write to the
- * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
- * Boston, MA 02110-1301, USA.
+ * You should have received a copy of the GNU Lesser General Public License along with this library;
+ * if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
+
+/*
+  * Author: River,Li
+  * Email: river.li@intel.com
+  * Date: 2018.10
+  */
 
 #ifndef __GST_WS_SINK_H__
 #define __GST_WS_SINK_H__
