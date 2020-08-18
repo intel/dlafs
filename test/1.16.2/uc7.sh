56b460fb30225798597ebed929ccd7a969f54a4e
cheny2 <yangyang.chen@intel.com>
2020-08-18 12:51:50 +0800

    - Update test scripts and IR bins.

--- test/1.16.2/uc7.sh
+++ test/1.16.2/uc7.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 rm -f .algocache.register
 export HDDL_PLUGIN=HDDL
-gst-launch-1.0 filesrc location=../indoor.mp4 ! qtdemux ! h264parse ! mfxh264dec ! cvdlfilter name=cvdlfilter0 algopipeline="yolov2tiny ! reid" ! resconvert name=resconvert0 resconvert0.src_txt ! ipcsink name=ipcsink_txt location=metadata.json resconvert0.src_pic ! mfxjpegenc ! ipcsink name=ipcsink_pic
+GST_LAUNCH=/opt/intel/openvino_2020.4.287/data_processing/gstreamer/bin/gst-launch-1.0
+${GST_LAUNCH} filesrc location=../indoor.mp4 ! qtdemux ! h264parse ! mfxh264dec ! cvdlfilter name=cvdlfilter0 algopipeline="yolov2tiny ! reid" ! resconvert name=resconvert0 resconvert0.src_txt ! ipcsink name=ipcsink_txt location=metadata.json resconvert0.src_pic ! mfxjpegenc ! ipcsink name=ipcsink_pic
--- test/1.16.2/uc8.sh
+++ test/1.16.2/uc8.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 rm -f .algocache.register
 export HDDL_PLUGIN=HDDL
-gst-launch-1.0 filesrc location=../car-detection.mp4 ! qtdemux ! h264parse ! mfxh264dec ! cvdlfilter name=cvdlfilter0 algopipeline="vehicle-license-plate-detection-barrier-0106 ! vehicle-attributes-recognition-barrier-0039" ! resconvert name=resconvert0 resconvert0.src_txt ! ipcsink name=ipcsink_txt location=metadata.json resconvert0.src_pic ! mfxjpegenc ! ipcsink name=ipcsink_pic
+GST_LAUNCH=/opt/intel/openvino_2020.4.287/data_processing/gstreamer/bin/gst-launch-1.0
+${GST_LAUNCH} filesrc location=../car-detection.mp4 ! qtdemux ! h264parse ! mfxh264dec ! cvdlfilter name=cvdlfilter0 algopipeline="vehicle-license-plate-detection-barrier-0106 ! vehicle-attributes-recognition-barrier-0039" ! resconvert name=resconvert0 resconvert0.src_txt ! ipcsink name=ipcsink_txt location=metadata.json resconvert0.src_pic ! mfxjpegenc ! ipcsink name=ipcsink_pic
